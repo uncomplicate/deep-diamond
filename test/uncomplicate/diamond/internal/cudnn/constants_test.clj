c3c74ad6706a377446624469796cd6c24b0310ff
Dragan Djuric <dragandj@gmail.com>
2020-07-27 01:58:31 +0200

    Add cuDNN constants test.

---
+++ test/uncomplicate/diamond/internal/cudnn/constants_test.clj
@@ -0,0 +1,16 @@
+;;   Copyright (c) Dragan Djuric. All rights reserved.
+;;   The use and distribution terms for this software are covered by the
+;;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) or later
+;;   which can be found in the file LICENSE at the root of this distribution.
+;;   By using this software in any fashion, you are agreeing to be bound by
+;;   the terms of this license.
+;;   You must not remove this notice, or any other, from this software.
+
+(ns uncomplicate.diamond.internal.cudnn.constants-test
+  (:require [midje.sweet :refer [facts =>]]
+            [uncomplicate.diamond.internal.cudnn.constants
+             :refer [cudnn-data-type dec-data-type]])
+  (:import clojure.lang.ExceptionInfo java.nio.ByteBuffer))
+
+(facts "cuDNN format tests."
+       (count (remove identity (map #(= % (cudnn-data-type (dec-data-type %))) (range 0)))) => 0)
