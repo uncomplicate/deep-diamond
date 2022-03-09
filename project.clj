138307e4a5228eacfdb55a64312dde548bbfb3f1
Dragan Djuric <dragandj@gmail.com>
2022-03-09 22:35:44 +0100

    Upgrade dnnl to 2.5.2 and JCuda to 11.5.2.

--- project.clj
+++ project.clj
@@ -6,16 +6,16 @@
 ;;   the terms of this license.
 ;;   You must not remove this notice, or any other, from this software.
 
-(defproject uncomplicate/deep-diamond "0.22.0"
+(defproject uncomplicate/deep-diamond "0.23.0-SNAPSHOT"
   :description "Fast Clojure Deep Learning Library"
   :author "Dragan Djuric"
   :url "http://github.com/uncomplicate/deep-diamond"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [uncomplicate/neanderthal "0.43.3"]
-                 [org.bytedeco/dnnl-platform "2.3-1.5.6"]
-                 [org.jcuda/jcudnn "11.4.1"]]
+                 [uncomplicate/neanderthal "0.44.0-SNAPSHOT"]
+                 [org.bytedeco/dnnl-platform "2.5.2-1.5.7"]
+                 [org.jcuda/jcudnn "11.5.2"]]
 
   :profiles {:dev {:plugins [[lein-midje "3.2.1"]
                              [lein-codox "0.10.7"]]
@@ -24,7 +24,7 @@
                                  *assert* false
                                  *unchecked-math* :warn-on-boxed
                                  *print-length* 128}
-                   :dependencies [[midje "1.10.4"]
+                   :dependencies [[midje "1.10.5"]
                                   [org.clojure/data.csv "1.0.0"]]
 
                    :jvm-opts ^:replace ["-Dclojure.compiler.direct-linking=true"
