64ec176e3ac61616669b5ffc1be77d6831cfe50c
Dragan Djuric <dragandj@gmail.com>
2025-06-17 12:12:11 +0200

    Renamed deep-diamond-pc to deep-diamond-aot.

---
+++ deep-diamond-aot/test/uncomplicate/diamond/functional/examples/saving_and_loading.clj
@@ -0,0 +1,67 @@
+(ns uncomplicate.diamond.functional.examples.saving-and-loading
+  (:require [midje.sweet :refer [facts throws => =not=>]]
+            [clojure.java.io :as io]
+            [clojure.data.csv :as csv]
+            [clojure.string :as string]
+            [uncomplicate.commons
+             [core :refer [with-release let-release]]
+             [utils :refer [random-access channel]]]
+            [uncomplicate.neanderthal
+             [core :refer [entry! entry zero asum transfer transfer! view-vctr native view-ge
+                           cols mv! rk! raw col row nrm2 scal! ncols dim rows]]
+             [native :refer [native-float fv]]
+             [random :refer [rand-uniform!]]
+             [math :as math :refer [sqrt]]]
+            [uncomplicate.diamond
+             [tensor :refer [*diamond-factory* tensor connector transformer
+                             desc revert shape input output view-tz batcher]]
+             [dnn :refer [sum activation inner-product fully-connected network init! train! cost rnn abbreviate]]
+             [native :refer [map-tensor]]]
+            [uncomplicate.diamond.internal.dnnl.factory :refer [dnnl-factory]]))
+
+(with-release [td1 (desc [2 3] :float :nc)
+               tf1 (random-access "td1.tz")
+               tz0 (tensor *diamond-factory* [2 3] :float :nc)
+               tz1 (map-tensor tf1 td1)
+               tz2 (map-tensor tf1 td1)
+               tz3 (entry! (zero tz0) 3)]
+
+  (facts "Saving and loading a tensor to/from a file."
+         (entry! tz1 0) => tz0
+         (entry! tz1 1) =not=> tz0
+         tz2 => tz1
+         (entry! tz2 2) => tz1
+         tz1 =not=> tz0
+         (transfer! tz3 tz1) => tz1
+         (asum tz2) => 18.0))
+
+(with-release [network-blueprint (network (desc [512 1 28 28] :float :nchw)
+                                          [(fully-connected [512] :relu)
+                                           (fully-connected [10] :sigmoid)])
+               net1 (init! (network-blueprint :adam))
+               net2 (network-blueprint :adam)
+               nfc1 (channel (random-access "network.trained"))]
+  (facts "Saving and loading and network by transferring it to/from a Java NIO file channel."
+         net1 =not=> net2
+         (transfer! net1 nfc1) => nfc1
+         (transfer! nfc1 net2)
+         net1 => net2))
+
+(with-release [network-blueprint (network (desc [4 2 1] :float :tnc)
+                                          [(rnn [4] :gru)
+                                           (rnn 2)
+                                           (abbreviate)
+                                           (fully-connected [2] :relu)
+                                           (fully-connected [1] :linear)])
+               net1 (init! (network-blueprint :adam))
+               net2 (init! (network-blueprint :adam) :zero)
+               nfc1 (channel (random-access "network2.trained"))]
+  (facts "Saving and loading an RNN network by transferring it to/from a Java NIO file channel."
+         net1 =not=> net2
+         (transfer! (range 2 20) (input net1))
+         (net1)
+         (transfer! net1 nfc1) => nfc1
+         (transfer! nfc1 net2) => net1
+         (transfer! (input net1) (input net2))
+         (net2) => (output net1)
+         net2 => net1))
---
+++ deep-diamond-aot/test/uncomplicate/diamond/functional/mnist/mnist_convolutional_test.clj
@@ -0,0 +1,87 @@
+(ns uncomplicate.diamond.functional.mnist.mnist-convolutional-test
+  (:require [midje.sweet :refer [facts throws => roughly]]
+            [uncomplicate.commons
+             [core :refer [with-release]]
+             [utils :refer [random-access channel]]]
+            [uncomplicate.neanderthal
+             [core :refer [transfer! native]]]
+            [uncomplicate.diamond
+             [tensor :refer [tensor desc with-diamond]]
+             [native :refer [map-tensor]]
+             [dnn :refer [dense convo pooling dropout network init! train! infer! batch-norm]]
+             [metrics :refer [classification-metrics]]]
+            [uncomplicate.diamond.internal.dnnl.factory :refer [dnnl-factory]]
+            [uncomplicate.diamond.internal.cudnn.factory :refer [cudnn-factory]]
+            [uncomplicate.diamond.functional.mnist.mnist-classification-test :as mnist]))
+
+(def train-images-file (random-access "data/mnist/train-images.idx3-ubyte"))
+(def train-labels-file (random-access "data/mnist/train-labels.idx1-ubyte"))
+(def test-images-file (random-access "data/mnist/t10k-images.idx3-ubyte"))
+(def test-labels-file (random-access "data/mnist/t10k-labels.idx1-ubyte"))
+
+(def train-images (map-tensor train-images-file [60000 1 28 28] :uint8 :nchw :read 16))
+(def train-labels (map-tensor train-labels-file [60000] :uint8 :x :read 8))
+(def test-images (map-tensor test-images-file [10000 1 28 28] :uint8 :nchw :read 16))
+(def test-labels (map-tensor test-labels-file [10000] :uint8 :x :read 8))
+
+(def train-labels-float (transfer! train-labels (tensor [60000] :float :x)))
+(def y-train (mnist/enc-categories train-labels-float))
+(def test-labels-float (transfer! test-labels (tensor [10000] :float :x)))
+(def y-test (mnist/enc-categories test-labels-float))
+
+(with-diamond dnnl-factory []
+  (with-release [net-bp (network (desc [128 1 28 28] :float :nchw)
+                                 [(convo [32] [3 3] :relu)
+                                  (convo [64] [3 3] :relu)
+                                  (pooling [2 2] :max)
+                                  (dropout)
+                                  (dense [128] :relu)
+                                  ;;(batch-norm)
+                                  (dropout)
+                                  (dense [10] :softmax)])
+                 net (init! (net-bp :adam))
+                 net-infer (net-bp)]
+    (facts "MNIST classification tests."
+           (time (train! net train-images y-train :crossentropy 2 [])) => (roughly 0.0 0.03)
+           (transfer! net net-infer)
+           (with-release [inf (infer! net-infer test-images)
+                          pred (mnist/dec-categories inf)
+                          metrics (:metrics (classification-metrics test-labels-float pred))]
+             (:accuracy metrics) => (roughly 0.98 0.05)
+             (:f1 metrics) => (roughly 0.985 0.005)
+             (take 8 pred) => (list 7.0 2.0 1.0 0.0 4.0 1.0 4.0 9.0)))))
+
+;; "Elapsed time: 52966.299469 msecs"
+;; "Elapsed time: 33227.93959 msecs" with new DNNL 2025...
+;; "Elapsed time: 17423.55197 msecs" with DNNL 7.7.3!
+
+(with-diamond cudnn-factory []
+  (with-release [x-mb-tz (tensor [128 1 28 28] :float :nchw)
+                 y-mb-tz (tensor [128 10] :float :nc)
+                 net-bp (network x-mb-tz
+                                 [(convo [32] [3 3] :relu)
+                                  (convo [64] [3 3] :relu)
+                                  (pooling [2 2] :max)
+                                  (dropout)
+                                  (dense [128] :relu)
+                                  (dropout)
+                                  (dense [10] :softmax)])
+                 net (init! (net-bp x-mb-tz :adam))
+                 net-infer (net-bp x-mb-tz)
+                 train-images (transfer! train-images (tensor [60000 1 28 28] :float :nchw))
+                 y-train (transfer! y-train (tensor [60000 10] :float :nchw))
+                 test-images (transfer! test-images (tensor [10000 1 28 28] :float :nchw))
+                 y-test (transfer! y-test (tensor [10000 10] :float :nchw))]
+    (facts "cuDNN MNIST classification tests."
+           (time (train! net train-images y-train :crossentropy 2 [])) => (roughly 0.0 0.2)
+           (transfer! net net-infer)
+           (with-release [inf (infer! net-infer test-images)
+                          native-inf (native inf)
+                          pred (mnist/dec-categories native-inf)
+                          metrics (:metrics (classification-metrics test-labels-float pred))]
+             (:accuracy metrics) => (roughly 0.95 0.02)
+             (:f1 metrics) => (roughly 0.95 0.02)
+             (take 8 pred) => (list 7.0 2.0 1.0 0.0 4.0 1.0 4.0 9.0)))))
+
+;; "Elapsed time: 3487.728516 msecs"
+;; "Elapsed time: 2341.201096 msecs" with new cuDNN 2025...
