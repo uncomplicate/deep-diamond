b5c2b37c6b3aebacf21a5216869342989d22be0a
Dragan Djuric <dragandj@gmail.com>
2025-09-30 15:06:56 +0200

    Release 0.39.0.

--- examples/hello-world-aot/project.clj
+++ examples/hello-world-aot/project.clj
@@ -1,8 +1,8 @@
-(defproject hello-world-aot "0.37.0"
+(defproject hello-world-aot "0.39.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.12.2"]
-                 [uncomplicate/deep-diamond "0.37.0"]]
+  :dependencies [[org.clojure/clojure "1.12.3"]
+                 [uncomplicate/deep-diamond "0.39.0"]]
 
   ;; uncomplicate/deep-diamond is AOT compiled for fast loading and developer convenience, which
   ;; might cause issues since it freezes org.clojure/core.async to the specific version (see ClojureCUDA).
@@ -14,14 +14,14 @@
              :linux {:dependencies [[org.uncomplicate/neanderthal-mkl "0.57.1"]
                                     [org.bytedeco/mkl "2025.2-1.5.12" :classifier "linux-x86_64-redist"]
                                     ;; optional, if you want GPU computing with CUDA. Beware: the cuda redist jars are very large!
-                                    [org.uncomplicate/deep-diamond-cuda "0.37.0"]
+                                    [org.uncomplicate/deep-diamond-cuda "0.39.0"]
                                     [org.bytedeco/cuda "12.9-9.10-1.5.13-20250913.041224-9" :classifier "linux-x86_64-redist"]
                                     [org.bytedeco/cuda "12.9-9.10-1.5.13-20250913.041224-9" :classifier "linux-x86_64-redist-cublas"]
                                     [org.bytedeco/cuda "12.9-9.10-1.5.13-20250913.041224-9" :classifier "linux-x86_64-redist-cudnn"]]}
              :windows {:dependencies [[org.uncomplicate/neanderthal-mkl "0.57.1"]
                                       [org.bytedeco/mkl "2025.2-1.5.12" :classifier "windows-x86_64-redist"]
                                       ;; optional, if you want GPU computing with CUDA. Beware: the cuda redist jars are very large!
-                                      [org.uncomplicate/deep-diamond-cuda "0.37.0"]
+                                      [org.uncomplicate/deep-diamond-cuda "0.39.0"]
                                       [org.bytedeco/cuda "12.9-9.10-1.5.13-20250913.041224-9" :classifier "windows-x86_64-redist"]
                                       [org.bytedeco/cuda "12.9-9.10-1.5.13-20250913.041224-9" :classifier "windows-x86_64-redist-cublas"]
                                       [org.bytedeco/cuda "12.9-9.10-1.5.13-20250913.041224-9" :classifier "windows-x86_64-redist-cudnn"]]}
@@ -29,8 +29,7 @@
                                      [org.bytedeco/openblas "0.3.30-1.5.12" :classifier "macosx-arm64"]]}}
 
   ;; Wee need this for the CUDA and/or DNNL binaries, which are not available in the Maven Central due to its huge size (3GB, vs 1GB limit)!
-  :repositories [["snapshots" "https://oss.sonatype.org/content/repositories/snapshots"]
-                 ["maven-central-snapshots" "https://central.sonatype.com/repository/maven-snapshots"]]
+  :repositories [["maven-central-snapshots" "https://central.sonatype.com/repository/maven-snapshots"]]
 
   ;; We need direct linking for properly resolving types in heavy macros and avoiding reflection warnings!
   :jvm-opts ^:replace ["-Dclojure.compiler.direct-linking=true"
--- examples/hello-world/project.clj
+++ examples/hello-world/project.clj
@@ -1,14 +1,14 @@
-(defproject hello-world-on-the-fly "0.37.0"
+(defproject hello-world-on-the-fly "0.39.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.12.2"]
+  :dependencies [[org.clojure/clojure "1.12.3"]
                  [org.uncomplicate/neanderthal-base "0.57.0"]
-                 [org.uncomplicate/deep-diamond-base "0.36.1"]
-                 [org.uncomplicate/deep-diamond-dnnl "0.36.1"]
-                 [org.uncomplicate/deep-diamond-bnns "0.36.1"]]
+                 [org.uncomplicate/deep-diamond-base "0.39.0"]
+                 [org.uncomplicate/deep-diamond-dnnl "0.39.0"]
+                 [org.uncomplicate/deep-diamond-bnns "0.39.0"]]
 
-  ;; If you'd like AOT compiled Neanderthal for fast namespace loading (1-2 seconds instead of 20),
-  ;; use uncomplicate/neanderthal (Linux or Windors) or org.uncomplicate/neanderthal-apple (MacOS arm64 or x86_64)
+  ;; If you'd like AOT compiled Deep Diamond for fast namespace loading (1-2 seconds instead of 20),
+  ;; see hello-world-aot example for reference.
 
   ;; Practically, all platform specific dependencies are optional.
   ;; You can use either MKL, OpenBLAS, or Accelerate for CPU computing as you wish (when your hardware supports them)
@@ -17,13 +17,13 @@
              :default/all {:dependencies [[org.bytedeco/openblas "0.3.30-1.5.12"]]}
              :linux {:dependencies [[org.uncomplicate/neanderthal-mkl "0.57.1"]
                                     [org.bytedeco/mkl "2025.2-1.5.12" :classifier "linux-x86_64-redist"]
-                                    [org.uncomplicate/deep-diamond-cuda "0.37.0"]
+                                    [org.uncomplicate/deep-diamond-cuda "0.39.0"]
                                     [org.bytedeco/cuda "12.9-9.10-1.5.13-20250913.041224-9" :classifier "linux-x86_64-redist"]
                                     [org.bytedeco/cuda "12.9-9.10-1.5.13-20250913.041224-9" :classifier "linux-x86_64-redist-cublas"]
                                     [org.bytedeco/cuda "12.9-9.10-1.5.13-20250913.041224-9" :classifier "linux-x86_64-redist-cudnn"]]}
              :windows {:dependencies [[org.uncomplicate/neanderthal-mkl "0.57.1"]
                                       [org.bytedeco/mkl "2025.2-1.5.12" :classifier "windows-x86_64-redist"]
-                                      [org.uncomplicate/deep-diamond-cuda "0.37.0"]
+                                      [org.uncomplicate/deep-diamond-cuda "0.39.0"]
                                       [org.bytedeco/cuda "12.9-9.10-1.5.13-20250913.041224-9" :classifier "windows-x86_64-redist"]
                                       [org.bytedeco/cuda "12.9-9.10-1.5.13-20250913.041224-9" :classifier "windows-x86_64-redist-cublas"]
                                       [org.bytedeco/cuda "12.9-9.10-1.5.13-20250913.041224-9" :classifier "windows-x86_64-redist-cudnn"]]}
