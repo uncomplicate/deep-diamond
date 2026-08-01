844dc23395ce3eee27acdcaea94a242b6a8e27e7
Dragan Djuric <dragandj@gmail.com>
2026-08-01 02:10:45 +0200

    Upgrade Neanderthal to 0.65.0.

--- deep-diamond-aot/project.clj
+++ deep-diamond-aot/project.clj
@@ -6,21 +6,21 @@
 ;;   the terms of this license.
 ;;   You must not remove this notice, or any other, from this software.
 
-(defproject uncomplicate/deep-diamond "0.46.0"
+(defproject uncomplicate/deep-diamond "0.46.1"
   :description "Fast Clojure Deep Learning Library"
   :author "Dragan Djuric"
   :url "http://github.com/uncomplicate/deep-diamond"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.12.5"]
-                 [org.uncomplicate/neanderthal-base "0.64.0"]
-                 [org.uncomplicate/neanderthal-mkl "0.64.0"]
-                 [org.uncomplicate/neanderthal-cuda "0.64.0"]
-                 [org.uncomplicate/neanderthal-accelerate "0.64.0"]
-                 [org.uncomplicate/deep-diamond-base "0.46.0"]
-                 [org.uncomplicate/deep-diamond-dnnl "0.46.0"]
-                 [org.uncomplicate/deep-diamond-cuda "0.46.0"]
-                 [org.uncomplicate/deep-diamond-bnns "0.46.0"]]
+                 [org.uncomplicate/neanderthal-base "0.65.0"]
+                 [org.uncomplicate/neanderthal-mkl "0.65.0"]
+                 [org.uncomplicate/neanderthal-cuda "0.65.0"]
+                 [org.uncomplicate/neanderthal-accelerate "0.65.0"]
+                 [org.uncomplicate/deep-diamond-base "0.46.1"]
+                 [org.uncomplicate/deep-diamond-dnnl "0.46.1"]
+                 [org.uncomplicate/deep-diamond-cuda "0.46.1"]
+                 [org.uncomplicate/deep-diamond-bnns "0.46.1"]]
 
   :aot [uncomplicate.neanderthal.internal.cpp.structures
         uncomplicate.neanderthal.internal.cpp.factory
